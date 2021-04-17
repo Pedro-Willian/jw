2d59ca802dd9dad91267468d3690d7411f3b278c
Eduardo Dallmann <eduardo.dallmann@ambevtech.com.br>
2021-04-17 08:25:43 -0300

    testes eslint

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,6 @@ import { Module } from '@nestjs/common';
 import { ConfigModule, ConfigService } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { AuthModule } from './auth/auth.module';
 import { Congregacao } from './congregacao/congregacao.entity';
 import { CongregacaoModule } from './congregacao/congregacao.module';
@@ -31,7 +30,7 @@ import { PublicadorModule } from './publicador/publicador.module';
     CongregacaoModule,
   ],
   controllers: [AppController],
-  providers: [AppService, ConfigService],
+  providers: [ConfigService],
   exports: [ConfigService],
 })
 export class AppModule {}
