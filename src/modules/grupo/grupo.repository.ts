1291f31df600e4a0eaeadbc3d9b03ff7e181534e
Eduardo Dallmann <eduardo.dallmann@ambevtech.com.br>
2021-04-21 00:06:28 -0300

    #14 alteração response

--- src/modules/grupo/grupo.repository.ts
+++ src/modules/grupo/grupo.repository.ts
@@ -5,6 +5,17 @@ import { Grupo } from './grupo.entity';
 export class GrupoRepository extends Repository<Grupo> {
   async listGrupo(congregacaoId: string) {
     const [grupos, total] = await this.createQueryBuilder('grupo')
+      .select([
+        'grupo',
+        'localizacao',
+        'dirigente.id',
+        'dirigente.nomeExibicao',
+        'ajudante.id',
+        'ajudante.nomeExibicao',
+      ])
+      .leftJoin('grupo.localizacao', 'localizacao')
+      .leftJoin('grupo.dirigente', 'dirigente')
+      .leftJoin('grupo.ajudante', 'ajudante')
       .where('grupo.congregacao = :congregacaoId', { congregacaoId })
       .getManyAndCount();
 
