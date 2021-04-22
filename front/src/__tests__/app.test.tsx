5a2873bbaa163eae022f1036606830740153016b
Eduardo Dallmann <eduardo.dallmann@ambevtech.com.br>
2021-04-21 23:23:38 -0300

    commit base front

---
+++ front/src/__tests__/app.test.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from 'app';
+
+describe('<App />', () => {
+  it('render', async () => {
+    render(<App />);
+    // expect(linkElement).toBeInTheDocument();
+  });
+});
---
+++ front/src/test-utils/setup-tests.ts
@@ -0,0 +1,34 @@
+import '@testing-library/jest-dom';
+
+Object.defineProperty(window, 'matchMedia', {
+  writable: true,
+  value: jest.fn().mockImplementation((query) => ({
+    matches: false,
+    media: query,
+    onchange: null,
+    addListener: jest.fn(), // deprecated
+    removeListener: jest.fn(), // deprecated
+    addEventListener: jest.fn(),
+    removeEventListener: jest.fn(),
+    dispatchEvent: jest.fn(),
+  })),
+});
+
+jest.mock('react-router-dom', () => ({
+  useLocation: jest.fn().mockReturnValue({
+    pathname: '/another-route',
+    search: '',
+    hash: '',
+    state: null,
+    key: '5nvxpbdafa',
+  }),
+  useHistory: () => ({
+    push: jest.fn(),
+  }),
+}));
+
+const mockDispatch = jest.fn();
+jest.mock('react-redux', () => ({
+  useSelector: jest.fn(),
+  useDispatch: () => mockDispatch,
+}));
