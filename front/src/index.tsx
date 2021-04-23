be032eb69b698ab2c5aa82bbe83eb4daf2676bf4
Eduardo Dallmann <eduardo.dallmann@ambevtech.com.br>
2021-04-23 15:56:36 -0300

    change css

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import 'antd/dist/antd.css';
 import { App } from 'app';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
